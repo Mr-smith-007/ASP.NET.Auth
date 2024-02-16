cc031b3af87967591794fc955c2139ba09b644f3
Mr-Smith-007 <mr_smith_007@mail.ru>
2024-02-16 11:06:32 +0300

    добавлен класс UserViewModel

---
+++ ASP.NET.Auth/UserViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net.Mail;
+
+namespace ASP.NET.Auth
+{
+    public class UserViewModel
+    {
+        public Guid Id { get; set; }
+        public string FullName { get; set; }
+        public bool FromRussia { get; set; }
+
+        public UserViewModel(User user)
+        {
+            Id = user.Id;
+            FullName = GetFullName(user.FirstName, user.LastName);
+            FromRussia = GetFromRussiaValue(user.Email);
+        }
+
+
+        public string GetFullName(string firstName, string lastName)
+        {
+            return String.Concat(firstName, " ", lastName);
+        }
+
+        public bool GetFromRussiaValue(string email)
+        {
+            MailAddress mailAddress = new MailAddress(email);
+
+            if (mailAddress.Host.Contains(".ru"))
+                return true;
+            return false;
+        }
+    }
+}
